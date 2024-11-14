383f04d13f75b9bb64943c7b120fa661bd8103ad
VladislavNikolaev0 <mr.nikolaev.vladislav.valen@mail.ru>
2024-11-15 00:01:11 +0300

    added pop up sing up

--- resiToStore/main/static/main/js/script.js
+++ resiToStore/main/static/main/js/script.js
@@ -1,4 +1,3 @@
-
 const htmlTemplate = `
     <div class="signIn">
         <div class="closeBtn">&times;</div>
@@ -20,6 +19,28 @@ const htmlTemplate = `
             </div>
         </div>
     </div>
+
+    <div class="signUp">
+    <div class="closeBtn">&times;</div>
+    <div class="formReg">
+        <h2>Регистрация</h2>
+        <div class="formRegElement">
+            <label for="emailReg">Почта</label>
+            <input type="text" id="emailReg" placeholder="example@gmail.com">
+        </div>
+        <div class="formRegElement">
+            <label for="passwordReg">Пароль</label>
+            <input type="password" id="passwordReg" placeholder="Пароль">
+        </div>
+        <div class="formRegElement">
+            <label for="passwordRegRepeat">Повторите пароль</label>
+            <input type="password" id="passwordRegRepeat" placeholder="Повторите пароль">
+        </div>
+        <div class="formRegElement">
+            <button class="signUpBtn">Регистрация</button>
+        </div>
+    </div>
+</div>l>
 `;
 
 document.body.insertAdjacentHTML('beforeend', htmlTemplate);
@@ -30,4 +51,12 @@ document.getElementById("show-login").addEventListener('click', function() {
 
 document.querySelector(".signIn .closeBtn").addEventListener('click', function() {
     document.querySelector(".signIn").classList.remove("activate");
+});
+
+document.getElementById("show-reg").addEventListener('click', function() {
+    document.querySelector(".signUp").classList.add("activate");
+});
+
+document.querySelector(".signUp .closeBtn").addEventListener('click', function() {
+    document.querySelector(".signUp").classList.remove("activate");
 });
